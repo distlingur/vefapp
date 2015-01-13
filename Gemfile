b862268624afb2037e0872a85879f9d9edbedd5b
distlingur <distlingur@gmail.com>
2015-01-13 20:28:16 +0000

    lala

--- Gemfile
+++ Gemfile
@@ -26,6 +26,8 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'font-awesome-sass'
 gem 'pg'
+gem 'mail_form'
+gem 'simple_form'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -2,20 +2,17 @@ class ContactController < ApplicationController
   def index
   end
   def new
-    @message = Message.new
+    @contact = Contact.new
   end
 
   def create
-    @message = Message.new(params[:message])
-    
-    if @message.valid?
-      NotificationsMailer.new_message(@message).deliver
-      redirect_to(root_path, :notice => "Message was successfully sent.")
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
     else
-      flash.now.alert = "Please fill all fields."
+      flash.now[:error] = 'Cannot send message.'
       render :new
     end
   end
-
 end
-
