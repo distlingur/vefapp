e92cc72a32479cd407be2dcacbc1f4687f99359b
distlingur <distlingur@gmail.com>
2015-01-14 16:18:07 +0000

    held se lagad

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "prófarkalestur",
+      :subject => "profarkalestur",
       :to => "ahprofork@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
