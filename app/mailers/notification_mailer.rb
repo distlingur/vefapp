e034b495e92629999ffe7b6cf3cb5805d4f16d08
distlingur <distlingur@gmail.com>
2015-03-02 23:28:35 +0000

    sma fix

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,10 +1,5 @@
 class NotificationMailer < ApplicationMailer
 default :from => "distlingur@gmail.com"
-  default :to => "distlingur@gmail.com"
 
-  def new_message(message)
-    @message = message
-    mail(:subject => "[YourWebsite.tld] #{message.subject}")
-  end
 
 end
